9398af67667d855ab98c790767ecb60b0ec92dac
lukasz94w <lukasz94w@wp.pl>
2021-10-03 16:16:51 +0200

    Showing simple list of posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NavigationHeaderComponent } from './navigation-header/navigation-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostListComponent,
+    NavigationHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
